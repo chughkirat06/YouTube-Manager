c0d01e6e65c3ddc3825e9680e93df846ed5726d5
chughkirat06 <chughkirat06@gmail.com>
2024-06-03 17:00:06 -0400

    Update and delete functionality added

--- youtuube-manager.py
+++ youtuube-manager.py
@@ -26,10 +26,24 @@ def add_video(videos):
     save_data_helper(videos)
 
 def update_video(videos):
-    pass
+    list_all_videos(videos)
+    index = int(input("Enter the video number to update: "))
+    if 1 <= index <= len(videos):
+        name = input("Enter the new video name: ")
+        time = input("Enter the new video time: ")
+        videos[index-1] = {'name': name, 'time': time}
+        save_data_helper(videos)
+    else:
+        print("Invalid video index selected.")
 
 def delete_video(videos):
-    pass
+    list_all_videos(videos)
+    index = int(input("Enter the video number to be deleted: "))
+    if 1 <= index <= len(videos):
+        del videos[index-1]
+        save_data_helper(videos)
+    else:
+        print("Invalid video index selected.")
 
 def main():
     videos = load_data()
