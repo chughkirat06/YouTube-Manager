2ff12d74be5738dfe93bdf17046d0c5395b8fd4b
chughkirat06 <chughkirat06@gmail.com>
2024-06-03 17:16:24 -0400

    Changes and improvements

--- youtuube-manager.py
+++ youtuube-manager.py
@@ -1,6 +1,10 @@
 import json
 
 def load_data():
+    """
+    Load video data from the file.
+    If the file does not exist, return an empty list.
+    """
     try:
         with open('youtube.txt', 'r') as file:
             return json.load(file)
@@ -8,24 +12,38 @@ def load_data():
         return []
 
 def save_data_helper(videos):
+    """
+    Save video data to the file.
+    """
     with open('youtube.txt', 'w') as file:
         json.dump(videos, file)
 
 def list_all_videos(videos):
-    print('\n')
-    print('*' * 70)
-    for index, video in enumerate(videos, start=1):
-        print(f"{index}. Name: {video['name']}, Duration: {video['time']}")
-    print('\n')
-    print('*' * 70)
+    """
+    Print a list of all the videos.
+    """
+    print('\n' + '*' * 70)
+    if videos:
+        for index, video in enumerate(videos, start=1):
+            print(f"{index}. Name: {video['name']}, Duration: {video['time']}")
+    else:
+        print("No videos found.")
+    print('*' * 70 + '\n') 
 
 def add_video(videos):
+    """
+    Add a new video to the list.
+    """
     name = input("Enter video name: ")
     time = input("Enter video time: ")
     videos.append({'name': name, 'time': time})
     save_data_helper(videos)
+    print("Video added successfully.")
 
 def update_video(videos):
+    """
+    Update the details of an existing video.
+    """
     list_all_videos(videos)
     index = int(input("Enter the video number to update: "))
     if 1 <= index <= len(videos):
@@ -33,19 +51,30 @@ def update_video(videos):
         time = input("Enter the new video time: ")
         videos[index-1] = {'name': name, 'time': time}
         save_data_helper(videos)
+        print("Video updated successfully.")
     else:
         print("Invalid video index selected.")
 
 def delete_video(videos):
+    """
+    Delete a video from the list.
+    """
     list_all_videos(videos)
-    index = int(input("Enter the video number to be deleted: "))
-    if 1 <= index <= len(videos):
-        del videos[index-1]
-        save_data_helper(videos)
-    else:
-        print("Invalid video index selected.")
+    try:
+        index = int(input("Enter the video number to be deleted: "))
+        if 1 <= index <= len(videos):
+            del videos[index-1]
+            save_data_helper(videos)
+            print("Video deleted successfully.")
+        else:
+            print("Invalid video index selected.")
+    except ValueError:
+        print("Please enter a valid number.")
 
 def main():
+    """
+    Main function to run the YouTube video manager
+    """
     videos = load_data()
 
     while True:
@@ -69,7 +98,7 @@ def main():
             case '5':
                 break
             case _:
-                print("Invalid Choice.")
+                print("Invalid Choice. Please enter a number between 1 and 5.")
 
 if __name__ == '__main__':
     main()
