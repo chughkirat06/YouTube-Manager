cb9dd2d367b6f370f3a7c18de3c95b107fb917a6
chughkirat06 <chughkirat06@gmail.com>
2024-06-04 15:01:25 -0400

    Youtube video Manager app with MongoDB

--- Youtube_Manager_MongoDB/youtube_manager.py
+++ Youtube_Manager_MongoDB/youtube_manager.py
@@ -1,29 +1,71 @@
-# mongodb+srv://youtubepy:<youtubepy>@cluster0.y1lp9yp.mongodb.net/
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 # Setup mongo db client
-client = MongoClient("mongodb+srv://youtubepy:<youtubepy>@cluster0.y1lp9yp.mongodb.net/")
+try:
+    client = MongoClient("mongodb+srv://{YOURUSERNAME}:{YOURPASSWORD}@cluster0.y1lp9yp.mongodb.net/")
+except Exception as e:
+    print(str(e))
 
 # Database name
 db = client["ytmanager"]
 video_collection = db["videos"]
-
-# print(video_collection)
+print(video_collection)
 """
 Collection(Database(MongoClient(host=['ac-boiuokx-shard-00-01.y1lp9yp.mongodb.net:27017', 'ac-boiuokx-shard-00-00.y1lp9yp.mongodb.net:27017', 'ac-boiuokx-shard-00-02.y1lp9yp.mongodb.net:27017'], document_class=dict, tz_aware=False, connect=True, authsource='admin', replicaset='atlas-1comw4-shard-0', tls=True), 'ytmanager'), 'videos')
 """
 
 def list_videos():
-    pass
+    """
+    List all videos in the collection.
+    """
+    videos = video_collection.find()
+    if videos:
+        print('\n' + '*' * 70)
+        for video in video_collection.find():
+            print(f"Id: {video['_id']}. Name: {video['name']} and Duration: {video['time']}")
+        print('*' * 70 + '\n')
+    else:
+        print("No videos found.")
 
 def add_video(name, time):
-    pass
+    """
+    Add a new video to the collection.
+
+    :param name: The name of the video.
+    :param time: The duration of the video.
+    """
+    video_collection.insert_one({"name": name, "time": time})
+    print("Video added successfully.")
 
 def update_video(video_id, new_name, new_time):
-    pass
+    """
+    Update an existing video's details.
+    
+    :param video_id: The ID of the video to update.
+    :param new_name: The new name of the video.
+    :param new_time: The new duration of the video.
+    """
+    video_collection.update_one(
+        {'_id': ObjectId(video_id)},
+        {"$set": {"name": new_name, "time": new_time}}
+    )
+    print("Video updated successfully.")
 
 def delete_video(video_id):
-    pass
+    """
+    Delete a video from the collection.
+    
+    :param video_id: The ID of the video to delete.
+    """
+    try:
+        result = video_collection.delete_one({"_id": ObjectId(video_id)})
+        if result.deleted_count:
+            print(f"Video {video_id} deleted successfully.")
+        else:
+            print(f"No video found with ID {video_id}.")
+    except Exception as e:
+        print(f"An error occured: {e}.")
 
 def main():
     while True:
